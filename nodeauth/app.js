5bd6c2285a51ba6c0072cc07435f12c14604c3b7
sanujsood <67072668+sanujsood@users.noreply.github.com>
2020-09-15 15:00:56 +0530

    Added question blog section

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -1,139 +1,225 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var expressValidator = require('express-validator');
-var multer = require('multer');
-var upload = multer({dest: './uploads'});
-var flash = require('connect-flash');
-var bcrypt = require('bcryptjs');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var expressValidator = require("express-validator");
+var multer = require("multer");
+var upload = multer({ dest: "./uploads" });
+var flash = require("connect-flash");
+var bcrypt = require("bcryptjs");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 const ejs = require("ejs");
 var db = mongoose.connection;
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-var routes = require('./routes/index');
-var users = require('./routes/users');
-//mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true , useUnifiedTopology:true});
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+var routes = require("./routes/index");
+var users = require("./routes/users");
+mongoose.createConnection("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 // app.set('public', path.join(__dirname, 'public'));
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 // app.use(express.static(__dirname + '/public'));
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
+//tinymice integration
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Handle Sessions
-app.use(session({
-  secret:'secret',
-  saveUninitialized: true,
-  resave: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function (param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value,
+      };
+    },
+  })
+);
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(flash());
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
+  res.locals.messages = require("express-messages")(req, res);
   next();
 });
 const postSchema = {
   title: String,
-  content: String
+  content: String,
 };
 
 const Post = mongoose.model("Post", postSchema);
 
-app.get("/", function(req, res){
-
-  Post.find({}, function(err, posts){
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
-      posts: posts
-      });
+      posts: posts,
+    });
   });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
   });
 
-
-  post.save(function(err){
-    if (!err){
-        res.redirect("/");
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
     }
   });
 });
 
-app.get("/posts/:postId", function(req, res){
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
 
-const requestedPostId = req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
     res.render("post", {
       title: post.title,
-      content: post.content
+      content: post.content,
+      postid: post._id,
     });
   });
-
 });
-app.get('*', function(req, res, next){
+
+app.get("/posts/:postId/edit", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("editpost", {
+      title: post.title,
+      content: post.content,
+      postid: post._id,
+    });
+  });
+});
+
+app.post("/posts/:postId/edit", (req, res) => {
+  const requestedId = req.params.id;
+  console.log(req.body);
+  Post.findOneAndUpdate(
+    {
+      _id: requestedId,
+    },
+    {
+      $set: {
+        title: req.body.postTitle,
+        content: req.body.postBody,
+      },
+    },
+    {
+      new: true,
+    },
+    (err, post) => {
+      if (!err) {
+        res.render("edit", {
+          title: post.title,
+          content: post.content,
+        });
+      }
+    }
+  );
+});
+
+//question
+
+const questionSchema = {
+  titleq: String,
+  contentq: String,
+};
+
+const Postq = mongoose.model("Postq", questionSchema);
+
+app.get("/question", function (req, res) {
+  res.render("question");
+});
+
+app.post("/question", function (req, res) {
+  const postq = new Postq({
+    titleq: req.body.postqTitle,
+    contentq: req.body.postqBody,
+  });
+
+  postq.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/questions/:titleq", function (req, res) {
+  const requestedquestionId = req.params.titleq;
+
+  Post.findOne({ titleq: requestedquestionId }, function (err, postq) {
+    res.render("postq", {
+      titleq: postq.titleq,
+      contentq: postq.contentq,
+    });
+  });
+});
+
+app.get("*", function (req, res, next) {
   res.locals.user = req.user || null;
   next();
 });
 
-
-app.use('/users', users);
+app.use("/users", users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -142,27 +228,26 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
-
 module.exports = app;
 
-app.listen(3000);
+app.listen(4000);
