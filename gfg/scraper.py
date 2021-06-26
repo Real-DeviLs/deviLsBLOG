4aea21e70af1cc5ffb7d2c2ec0eb15085afdb81b
Ubuntu <realdevils@realdevils2.5ujjw3jmviaufooalcjdxyfqab.bx.internal.cloudapp.net>
2021-06-26 10:43:42 +0000

    data from VM before crash

--- gfg/scraper.py
+++ gfg/scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+from .models import Leaderboard
 
 def fetchResponse(username):
     li = []
@@ -13,5 +14,6 @@ def fetchResponse(username):
             for div in soup.findAll('li', attrs={'class':'mdl-cell mdl-cell--6-col mdl-cell--12-col-phone'}):
                 li.append(div.find('a')['href'])
     except:
+#        Leaderboard.objects.filter(username=username).delete()
         return li,0
     return li, weekly_score
